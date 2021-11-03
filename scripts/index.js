e2e5eaa6d1eb6fd9d8334eed8b4d3464a3637f5c
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-11-03 08:43:14 +0530

    Progress

---
+++ scripts/index.js
@@ -0,0 +1,261 @@
+const courses=[{
+    title : "Global Doctor of Business Administration",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/SSBMlogo.png",
+    university: "Swiss school of businees and management",
+    category: "Management",
+    Duration: 36,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "MBA (Global)",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/deakinlogo.png",
+    university: "Deakin Business School",
+    category: "Management",
+    Duration: 24,
+    link : "#",
+    offer: "Trending"
+},
+{
+    title : "MBA (Global)- Specalisations",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/liverlogo.jpg",
+    university: "Liverpool Business School",
+    category: "Management",
+    Duration: 18,
+    link : "#",
+    offer: "Trending"
+},
+{
+    title : "Executive-MBA",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/SSBMlogo.png",
+    university: "Swiss school of businees and management",
+    category: "Management",
+    Duration: 12,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "Post Graduate Diploma in Management",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/birlalogo.png",
+    university: "Birla institute of Management",
+    category: "Management",
+    Duration: 18,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "Master of Science in Data Science",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/arizonalogo.png",
+    university: "University of Arizona",
+    category: "DataScience",
+    Duration: 18,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "Executive PG Program in Data Science",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT bangalore",
+    category: "DataScience",
+    Duration: 12,
+    link : "#",
+    offer: "Trending"
+},
+{
+    title : "Master of Science in Data Science",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "Liverpool john University",
+    category: "DataScience",
+    Duration: 12,
+    link : "#",
+    offer: "Trending"
+},
+{
+    title : "Professional Certificate in Data Science",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iim_kozhikode.png",
+    university: "IIM KOZHIKODE",
+    category: "DataScience",
+    Duration: 8,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "Advanced Certificate Programme in Data Science",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT bangalore",
+    category: "DataScience",
+    Duration: 8,
+    link : "#",
+    offer: ""
+},
+{
+    title : "Advanced Programme in Data Science",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT bangalore",
+    category: "DataScience",
+    Duration: 12,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "Python Programming Boot Camp",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/logo.jpg",
+    university: "UpGrad",
+    category: "DataScience",
+    Duration: 2,
+    link : "#",
+    offer: "New"
+},
+{
+    title : "Executive PG Programme in Machine Learning & AI",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT Bangalore",
+    category: "MachineLearning",
+    Duration: 13,
+    link : "#",
+    offer: "Trending"
+},
+{
+    title : "Master of Learning in Machine Learning & AI",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT Bangalore",
+    category: "MachineLearning",
+    Duration: 18,
+    link : "#",
+    offer:"",
+},
+{
+    title : "Master of Learning in Machine Learning & AI",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitm_logo.png",
+    university: "IIT Madras",
+    category: "MachineLearning",
+    Duration: 18,
+    link : "#",
+    offer:"",
+},
+{
+    title : "ADvance certificate in Machine Learning & AI",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitm_logo.png",
+    university: "IIT Madras",
+    category: "MachineLearning",
+    Duration: 18,
+    link : "#",
+    offer:"",
+},
+{
+    title : "Advanced Certificate in Machine Learning & Deep Learning",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT Bangalore",
+    category: "MachineLearning",
+    Duration: 18,
+    link : "#",
+    offer:"",
+},
+{
+    title : "Advanced Certificate in Machine Learning & Deep Learning",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/iitb_logo.png",
+    university: "IIT Bangalore",
+    category: "MachineLearning",
+    Duration: 18,
+    link : "#",
+    offer:"",
+},
+{
+    title : "Executive PG Program in Business Analytics",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/LIBA-logo.jpg",
+    university: "Loyola Institute of Business",
+    category: "BusinessAnalytics",
+    Duration: 12,
+    link : "#",
+    offer:"New",
+},
+{
+    title : "Global Master Certificate in Business Analytics",
+    image: "https://ik.imagekit.io/upgrad1/marketing-platform-assets/sprites%2Fimages/SSBM__1619611109162.png",
+    logo: "../images/michigan_logo.png",
+    university: "Michigan State University",
+    category: "BusinessAnalytics",
+    Duration: 12,
+    link : "#",
+    offer:"",
+},
+]
+var sidedropdown = document.getElementById("sidedropdown")
+function showcourse(value){
+    let data = courses.filter(el => el.category == value)
+    showondrop(data)
+}
+
+function showondrop(data){
+    sidedropdown.innerHTML = null;
+
+    data.forEach(({title,logo,Duration,university,link,offer}) => {
+        let div = document.createElement("div")
+        div.style.margin = "20px"
+        div.onclick = ()=>{
+            window.location.href = link;
+        }
+        div.setAttribute("class","border-black border-2 rounded flex hover:border-gs-red hover:bg-gray-100 ")
+        let img = document.createElement("img")
+        img.src = logo;
+        img.style.width = "20%"
+        img.style.height= "40px"
+        img.style.marginTop="20px"
+        let details = document.createElement("div")
+        details.style.width= "70%";
+        let h1 = document.createElement("h1")
+        h1.style.fontWeight= "bold"
+        h1.textContent = title;
+        let uni = document.createElement("p")
+        uni.style.fontSize = "12px"
+        uni.textContent = university;
+        let p = document.createElement("p")
+        p.innerText = "Duration-" + Duration +" Months";
+        p.style.fontSize= "12px"
+        let off = document.createElement("p")
+        off.style.margin = "5px"
+        off.style.padding = "2px"
+        off.style.width = "fit-content"
+        if(offer == "New"){
+            off.style.backgroundColor = "red";
+            off.style.color= "white"
+        }else if(offer == ""){
+            off.style.backgroundColor = "transparent";
+        }
+        
+        else{
+            off.style.backgroundColor = "yellow";
+            off.style.color= "red"
+        }
+        off.setAttribute("class","rounded")
+        let arrow = document.createElement("div")
+        arrow.innerHTML = "​&#8594;"
+        arrow.style.marginTop= "10%"
+        arrow.style.backgroundColor= "yellow"
+        arrow.setAttribute("class","text-3xl text-gray-400 h-0")
+        off.textContent = offer
+        details.append(h1,uni,p,off)
+        div.append(img,details,arrow)
+        sidedropdown.append(div)
+    });
+
+}
