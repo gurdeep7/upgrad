d49d43e4b9d4f4d8113edd799b9d0a1d015afd5e
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-12-14 19:57:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./index")
 
 const connect = require("./config/db")
 
-var port_number = 3000 || process.env.PORT;
+var port_number = process.env.PORT || 80;
 
 app.listen(port_number ,async()=>{  // do not add localhost here if you are deploying it
     await connect();
