99b99fffa1e35b86dac28a361b4fb6385162cf6c
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-12-14 17:09:36 +0530

    progress

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ const app = require("./index")
 
 const connect = require("./config/db")
 
-server.listen(3000,()=>{  // do not add localhost here if you are deploying it
+app.listen(3000,()=>{  // do not add localhost here if you are deploying it
     console.log("server listening to 3000 ");
 });
