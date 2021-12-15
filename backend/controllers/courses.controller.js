94f03f58738d1e9c17e150bab7ca409efd9c8f5f
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-12-15 15:33:01 +0530

    Update courses.controller.js

--- backend/controllers/courses.controller.js
+++ backend/controllers/courses.controller.js
@@ -4,7 +4,7 @@ const courses = require("../models/courses.model")
 
 const router = express.Router()
 
-router.post("/",async(req,res)=>{
+router.post("",async(req,res)=>{
    try {
         const Course =await courses.create(req.body)
 
@@ -15,9 +15,32 @@ router.post("/",async(req,res)=>{
 }
 })
 router.get("",async(req,res)=>{
-    console.log("work")
     try{
         const Course =await courses.find().lean().exec()
+        res.status(201).send(Course)
+    }catch(e){
+        res.status(500).json({status:e.message})
+    }
+})
+router.get("/:id",async(req,res)=>{
+    try{
+        const Course =await courses.findById(req.params.id).lean().exec()
+        res.status(201).send(Course)
+    }catch(e){
+        res.status(500).json({status:e.message})
+    }
+})
+router.delete("/:id",async(req,res)=>{
+    try{
+        const Course =await courses.findByIdAndDelete(req.params.id).lean().exec()
+        res.status(201).send(Course)
+    }catch(e){
+        res.status(500).json({status:e.message})
+    }
+})
+router.patch("/:id",async(req,res)=>{
+    try{
+        const Course =await courses.findByIdAndUpdate(req.params.id,req.body).lean().exec()
         res.status(201).send(Course)
     }catch(e){
         res.status(500).json({status:e.message})
