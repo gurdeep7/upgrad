29e9cc4554d964678d1625911ecdc6aa29a9e64c
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2021-12-15 09:21:35 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const courseController = require("./controllers/courses.controller")
 
+const cors = require("cors")
+
+app.use(cors())
+
 app.use(express.json())
 
 app.use("/course", courseController)
